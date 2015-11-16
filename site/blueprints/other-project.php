1c60a8aa8b3ec0d0e7ba73d712c5a50630771102
Lemon <lemon@hirelemon.com>
2015-11-16 12:33:24 -0600

    Tweet intent and GitHub repo are handled inside the content panel.
    Because the other method was stupid.

--- site/blueprints/other-project.php
+++ site/blueprints/other-project.php
@@ -15,43 +15,55 @@ fields:
     options:
       qe: Quite Exasperating
       irregular: Irregular
-      wrongest: The Wrongest Words
+      website: Website
+      merch: Merchandise
       else: Something Else
   date:
     label: Release Date
     type: date
     format: MM/DD/YYYY
-    width: 1/2
+    width: 1/4
   time:
     label: Release Time
     type: time
     interval: 1
-    width:1/2
-  cast:
-    label: Cast
-    type: tags
-    lowercase:true
-    icon: users
+    width:1/4
   show_image:
     label: Show image on page?
     type: toggle
     text: yes/no
     width:1/2
-  episode_file:
-    label: Episode File
-    type: url
-    icon: volume-up
-    width:1/2
+  cast:
+    label:Cast
+    type:tags
+    lowercase:true
+    icon:users
   text:
     label: Item Summary
     type: textarea
     icon: align-left
+  episode_file:
+    label: Episode File
+    type: url
+    placeholder: (if applicable)
+    icon: volume-up
+    width:1/2
+  github_repo:
+    label: GitHub Repo URL
+    type: url
+    placeholder: (if applicable)
+    icon: github
+    width:1/2
   tags:
     label: Project Tags
     lowercase:true
     type: tags
+  tweet_intent:
+    label: Custom Tweet Intent
+    type: url
+    placeholder: Use only if you want something different.
+    icon: retweet
   bonus_content:
     label: Bonus Content
-    placeholder: Raw HTML only, use only if completely necessary
     type:textarea
     icon:code
