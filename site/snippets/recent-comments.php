20d151846a1f185309f7455f461c623a18411a4c
Lemon <lemon@hirelemon.com>
2016-01-04 11:19:47 -0600

    Deleting useless files.

--- site/snippets/recent-comments.php
+++
@@ -1,3 +0,0 @@
-<div id="recentcomments" class="dsq-widget">
-  <script type="text/javascript" src="http://thefplus-dev.disqus.com/recent_comments_widget.js?num_items=5&hide_avatars=1&avatar_size=36&excerpt_length=200"></script>
-</div>
