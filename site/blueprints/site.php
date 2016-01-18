9b3a5ec3e97ffc665998cfd5fb2b4c974f8589ce
Lemon <lemon@hirelemon.com>
2016-01-18 14:17:46 -0600

    I have no idea what's in this commit
    Useful things maybe?

--- site/blueprints/site.php
+++ site/blueprints/site.php
@@ -13,15 +13,22 @@ fields:
   title:
     label: Title
     type:  text
-  author:
-    label: Author
-    type:  text
+  email:
+    label: Site Admin Email
+    type: email
+    required: true
+    width: 1/2
+  twitter_handle:
+    label: Twitter Handle (no @)
+    type: text
+    placeholder: (no @)
+    icon: twitter
+    width: 1/2
+    required: true
   description:
     label: Description
     type:  textarea
-  keywords:
-    label: Keywords
-    type:  tags
   copyright:
     label: Copyright
-    type:  textarea
+    type:  text
+  
