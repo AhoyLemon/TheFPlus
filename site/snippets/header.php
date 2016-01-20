327edd751fd1acc120462058cd8791d5fb5bfce0
Lemon <lemon@hirelemon.com>
2016-01-20 12:01:13 -0600

    That cachebuster isn't necessary anymore.

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -7,25 +7,25 @@
   <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
   <meta name="description" content="<?php echo excerpt($page->text()->xml(), 150) ?>">
   <!-- Favicons -->
-  <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png?v=062615">
-  <link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png?v=062615">
-  <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png?v=062615">
-  <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png?v=062615">
-  <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png?v=062615">
-  <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png?v=062615">
-  <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png?v=062615">
-  <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png?v=062615">
-  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png?v=062615">
-  <link rel="icon" type="image/png" href="/favicon-32x32.png?v=062615" sizes="32x32">
-  <link rel="icon" type="image/png" href="/android-chrome-192x192.png?v=062615" sizes="192x192">
-  <link rel="icon" type="image/png" href="/favicon-96x96.png?v=062615" sizes="96x96">
-  <link rel="icon" type="image/png" href="/favicon-16x16.png?v=062615" sizes="16x16">
-  <link rel="manifest" href="/manifest.json?v=062615">
-  <link rel="shortcut icon" href="/favicon.ico?v=062615">
+  <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">
+  <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+  <link rel="icon" type="image/png" href="/android-chrome-192x192.png" sizes="192x192">
+  <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
+  <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+  <link rel="manifest" href="/manifest.json">
+  <link rel="shortcut icon" href="/favicon.ico">
   <meta name="apple-mobile-web-app-title" content="The F Plus">
   <meta name="application-name" content="The F Plus">
   <meta name="msapplication-TileColor" content="#c0282d">
-  <meta name="msapplication-TileImage" content="/mstile-144x144.png?v=062615">
+  <meta name="msapplication-TileImage" content="/mstile-144x144.png">
   <meta name="theme-color" content="#c0282d">
   
   <!-- Twitter -->
