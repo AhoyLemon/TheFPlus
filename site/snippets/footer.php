ed5349630d54e0ea5057839c30750054c41b1167
Lemon <lemon@hirelemon.com>
2016-01-20 12:51:42 -0600

    fanart changes.

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,5 +1,5 @@
   <footer class="footer cf" role="contentinfo">
-  <script src="<?php echo url::home() ?>/assets/js/thefplus.js?v=11.22.15"></script>
+  <script src="<?php echo url::home() ?>/assets/js/thefplus.js?v=01.20.16"></script>
   <?php snippet('analytics') ?>
   <script src="<?php echo url::home() ?>/assets/js/newurl.js"></script>
   </footer>
