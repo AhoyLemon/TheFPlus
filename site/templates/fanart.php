2f33d8dbf52e7de8c14fefadf57775a802b495c1
Lemon <lemon@thefpl.us>
2016-03-16 20:40:45 -0500

    I hope this doesn't cause a merge conflict.

--- site/templates/fanart.php
+++ site/templates/fanart.php
@@ -1,7 +1,7 @@
 <?php snippet('header') ?>
 
-  <main class="main page" role="main">
-    <h1><?php echo $page->title(); ?></h1>
+  <main class="main edge-to-edge" role="main">
+    <h1 class="fanart-headline"><?php echo $page->title(); ?></h1>
 
     <article class="full default">
       <ul class="fanart-list">
@@ -10,7 +10,7 @@
         <?php $fa = explode("-", $fanart->filename()); ?>
         <li>
           <a href="/episode/<?php echo $fa[0]; ?>#AdditionalFun" class="fanart-link">
-            <img src="/fanart/<?php echo $fanart->filename(); ?>" class="fanart-thumb">
+            <img src="<?php echo $fanart->crop(250, 250)->url(); ?>" class="fanart-thumb">
           </a>
         </li>
       <?php endforeach; ?>
