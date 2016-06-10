dd6c3b525cc5dd07cffb4ae17f0e05d771ca4a06
Lemon <lemon@hirelemon.com>
2016-06-10 15:44:45 -0500

    First attempt at service-worker
    Working with
    https://github.com/GoogleChrome/samples/tree/gh-pages/service-worker/custom-offline-page
    to try to get a web install banner going.

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,6 +1,11 @@
   <footer class="footer">
   <script src="<?php echo url::home() ?>/assets/js/thefplus.js?v=01.20.16"></script>
   <?php snippet('analytics') ?>
+    <script>
+      if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/sw.js');
+      }
+    </script>
   <script src="<?php echo url::home() ?>/assets/js/newurl.js"></script>
   </footer>
 </body>
