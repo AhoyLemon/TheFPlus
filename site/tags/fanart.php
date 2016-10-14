bee40d8bd03681f62e00c8ab30072753b93e7ee1
Lemon <lemon@hirelemon.com>
2016-10-14 16:13:15 -0500

    clean

--- site/tags/fanart.php
+++ site/tags/fanart.php
@@ -5,10 +5,8 @@ kirbytext::$tags['fanart'] = array(
     'by'
   ),
   'html' => function($tag) {
-
     $picture = $tag->attr('fanart');
     $artist  = $tag->attr('by', 'someone');
-
     return '<figure class="fanart"> <img src="/fanart/' . $picture . '" /> <figcaption>by ' . $artist . '</figcaption></figure>';
   }
 );
