8f44e746fcff92041566d22fddd677d6190bca48
Lemon <lemon@thefpl.us>
2016-10-27 21:05:39 -0500

    Disguise Piwik

--- site/snippets/analytics.php
+++ site/snippets/analytics.php
@@ -11,15 +11,16 @@
 <!-- Piwik -->
 <script type="text/javascript">
   var _paq = _paq || [];
+  _paq.push(["setDomains", ["*.thefpl.us"]]);
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
-    var u="//thefpl.us/piwik/";
-    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    var u="//thefpl.us/analytics/";
+    _paq.push(['setTrackerUrl', u+'pwk.php']);
     _paq.push(['setSiteId', '1']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'pwk.js'; s.parentNode.insertBefore(g,s);
   })();
 </script>
-<noscript><p><img src="//thefpl.us/piwik/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+<noscript><p><img src="//thefpl.us/analytics/pwk.php?idsite=1" style="border:0;" alt="" /></p></noscript>
 <!-- End Piwik Code -->
