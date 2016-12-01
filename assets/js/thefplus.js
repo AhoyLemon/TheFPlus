d6d03497e51d5a9a7b873e261f0eac7be74643e0
Lemon <lemon@thefpl.us>
2016-11-30 19:33:12 -0600

    This removes GA.

--- assets/js/thefplus.js
+++ assets/js/thefplus.js
@@ -3,16 +3,16 @@
 function sendGA(c, a, l, v) {
   if (v) {
     _paq.push(['trackEvent', c, a, l, v]);
-    ga('send', 'event', { eventCategory: c, eventAction: a, eventLabel: l, eventValue:v });
-    console.log('CATEGORY: '+c+', ACTION:'+a+', LABEL:'+l+', VALUE:'+v);
+    //ga('send', 'event', { eventCategory: c, eventAction: a, eventLabel: l, eventValue:v });
+    //console.log('CATEGORY: '+c+', ACTION:'+a+', LABEL:'+l+', VALUE:'+v);
   } else if (l) {
     _paq.push(['trackEvent', c, a, l]);
-    ga('send', 'event', { eventCategory: c, eventAction: a, eventLabel: l });
-    console.log('CATEGORY: '+c+', ACTION:'+a+', LABEL:'+l);
+    //ga('send', 'event', { eventCategory: c, eventAction: a, eventLabel: l });
+    //console.log('CATEGORY: '+c+', ACTION:'+a+', LABEL:'+l);
   } else {
     _paq.push(['trackEvent', c, a]);
-    ga('send', 'event', { eventCategory: c, eventAction: a });
-    console.log('CATEGORY: '+c+', ACTION:'+a);
+    //ga('send', 'event', { eventCategory: c, eventAction: a });
+    //console.log('CATEGORY: '+c+', ACTION:'+a);
   }
 }
 
