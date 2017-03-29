58c0e193a1b24ed315bdc2fd13f5d32329dda8b1
Lemon <AhoyLemon@users.noreply.github.com>
2017-03-29 13:56:46 -0500

    Removing unused templates.

--- site/snippets/most-popular-tags.php
+++ site/snippets/most-popular-tags.php
@@ -1,7 +1,11 @@
-<?php $tagcloud = tagcloud(page('episode'), array('limit' => 20)) ?>
-  <h4 class="popular-tags">most popular tags</h4>
-  <ul class="tag-cloud">
+<?php $tagcloud = tagcloud(page('episode'), array('limit' => 20)); ?>
+<h4 class="popular-tags">most popular tags</h4>
+<ul class="tag-cloud">
   <?php foreach($tagcloud as $tag): ?>
-    <li><a href="<?php echo $tag->url() ?>"><?php echo $tag->name() ?></a></li>
+    <li>
+      <a href="<?php echo $tag->url() ?>">
+        <?php echo $tag->name() ?>
+      </a>
+    </li>
   <?php endforeach ?>
-  </ul>
+</ul>
