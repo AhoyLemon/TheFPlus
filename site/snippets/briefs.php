c6f7dca7707a6b045398a8a3fa74250f698130eb
Lemon <AhoyLemon@users.noreply.github.com>
2017-04-26 00:08:28 -0500

    24TH launched!

--- site/snippets/briefs.php
+++ site/snippets/briefs.php
@@ -4,63 +4,100 @@
   <?php foreach($articles as $article): ?>
   
     <?php if ($article->parent()->slug() == "episode") { ?>
-      <!-- EPISODE BRIEF -->
-      <article class="episode brief">
-        <header>
-          <h2 class="title">
-            <a href="<?php echo $article->url() ?>">
-              <span class="name">
-                <?php echo html($article->title()) ?>
+  
+      <?php if ($article->template() == "episode") { ?>
+        <!-- EPISODE BRIEF -->
+        <article class="episode brief">
+          <header>
+            <h2 class="title">
+              <a href="<?php echo $article->url() ?>">
+                <span class="name">
+                  <?php echo html($article->title()) ?>
+                </span>
+              </a>
+            </h2>
+            <time class="published released">
+              <span class="date">
+                <?php echo date('l, F jS Y', $article->date()) ?>
               </span>
+              @
+              <span class="time">
+                <?php echo date("g:ia", strtotime($article->time())) ?>
+              </span>
+            </time>
+          </header>
+          <?php if($image = $article->image()): ?>
+            <a class="image-holder" href="<?php echo $article->url() ?>" title="<?php echo html($article->title()) ?>">
+              <?php if ($article->cover() != "") { ?>
+                <img src="<?php echo $article->url() ?>/<?php echo $article->cover()->filename() ?>" class="cover" alt="<?php echo $article->title(); ?>" />
+              <?php } else { ?>
+                <img src="<?php echo $article->url() ?>/<?php echo $image->filename() ?>" class="cover" alt="<?php echo $article->title(); ?>" />
+              <?php } ?>
+              <?php if ($article->tags() != ""):
+                $etags = explode(",", $article->tags());
+              ?>
+                <div class="hover-cover">
+                  <ul class="tags">
+                    <?php foreach($etags as $etag): ?>
+                    <li><?php echo $etag ?></li>
+                    <?php endforeach ?>
+                  </ul>
+                </div>
+              <?php endif ?>
             </a>
-          </h2>
-          <time class="published released">
-            <span class="date">
-              <?php echo date('l, F jS Y', $article->date()) ?>
-            </span>
-            @
-            <span class="time">
-              <?php echo date("g:ia", strtotime($article->time())) ?>
-            </span>
-          </time>
-        </header>
-        <?php if($image = $article->image()): ?>
-          <a class="image-holder" href="<?php echo $article->url() ?>" title="<?php echo html($article->title()) ?>">
-            <?php if ($article->cover() != "") { ?>
-              <img src="<?php echo $article->url() ?>/<?php echo $article->cover()->filename() ?>" class="cover" alt="<?php echo $article->title(); ?>" />
-            <?php } else { ?>
-              <img src="<?php echo $article->url() ?>/<?php echo $image->filename() ?>" class="cover" alt="<?php echo $article->title(); ?>" />
-            <?php } ?>
-            <?php if ($article->tags() != ""):
-              $etags = explode(",", $article->tags());
-            ?>
-              <div class="hover-cover">
-                <ul class="tags">
-                  <?php foreach($etags as $etag): ?>
-                  <li><?php echo $etag ?></li>
-                  <?php endforeach ?>
-                </ul>
-              </div>
-            <?php endif ?>
-          </a>
-        <?php endif ?>
-        <summary>
-          <?php if ($article->cast() != ""):
-            $persons = explode(",", $article->cast());
-          ?>
-            <ul class="cast ridiculists">
-              <?php foreach($persons as $person): ?>
-                <li><?php echo $person ?></li>
-              <?php endforeach ?>
-            </ul>
           <?php endif ?>
-          <div class="content">
-            <p><?php echo excerpt($article->text(), 222) ?></p>
-          </div>
-        </summary>
-        <span class="episode-number"><?php echo $article->uid(); ?></span>
-        <a class="disqus-comment-count" href="<?php echo $article->url() ?>#disqus_thread" data-disqus-identifier="<?php echo $article->uri(); ?>"></a>
-      </article>
+          <summary>
+            <?php if ($article->cast() != ""):
+              $persons = explode(",", $article->cast());
+            ?>
+              <ul class="cast ridiculists">
+                <?php foreach($persons as $person): ?>
+                  <li><?php echo $person ?></li>
+                <?php endforeach ?>
+              </ul>
+            <?php endif ?>
+            <div class="content">
+              <p><?php echo excerpt($article->text(), 222) ?></p>
+            </div>
+          </summary>
+          <span class="episode-number"><?php echo $article->uid(); ?></span>
+          <a class="disqus-comment-count" href="<?php echo $article->url() ?>#disqus_thread" data-disqus-identifier="<?php echo $article->uri(); ?>"></a>
+        </article>
+      <?php } else if ($article->template() == "marathon") { ?>
+        <article class="episode brief marathon">
+          <header>
+            <h2 class="title">
+              <a href="<?php echo $article->url() ?>">
+                <span class="name">
+                  <?php echo html($article->title()) ?>
+                </span>
+              </a>
+            </h2>
+            <time class="published released">
+              <span class="date">
+                <?php echo date('l, F jS Y', $article->date()) ?>
+              </span>
+              @
+              <span class="time">
+                <?php echo date("g:ia", strtotime($article->time())) ?>
+              </span>
+            </time>
+          </header>
+          <a class="image-holder" href="<?php echo $article->url() ?>" title="<?php echo html($article->title()) ?>">
+            <img src="<?php echo $article->url() ?>/<?php echo $article->cover()->filename() ?>" class="cover" alt="<?php echo $article->title(); ?>" />
+          </a>
+          <summary>
+            <div class="content">
+              <ul>
+                <?php foreach ($article->hours()->toStructure() as $hour) { ?>
+                  <li><?php echo $hour->name(); ?></li>
+                <?php } ?>
+              </ul>
+            </div>
+          </summary>
+          <a class="disqus-comment-count" href="<?php echo $article->url() ?>#disqus_thread" data-disqus-identifier="<?php echo $article->uri(); ?>"></a>
+        </article>
+      <?php } ?>
     <?php } else if ($article->parent()->slug() == "also-made") { ?>
       <!-- ALSO MADE BRIEF -->
       <article class="also-made brief">
