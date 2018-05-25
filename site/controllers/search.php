d08dcb5664a7da53cabd6903237f0521d29bbf57
Lemon <AhoyLemon@users.noreply.github.com>
2018-05-25 11:11:16 -0500

    Search in tags as well
    Related to #23

--- site/controllers/search.php
+++ site/controllers/search.php
@@ -3,7 +3,7 @@
 return function($site, $pages, $page) {
 
   $query   = get('q');
-  $results = $site->search($query, 'title|text|featured_site|music_used');
+  $results = $site->search($query, 'title|text|featured_site|music_used|tags');
 
   return array(
     'query'   => $query,
