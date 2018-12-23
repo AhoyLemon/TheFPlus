458f4445b2e03ab13ccefd18c410fdfd76da831a
Lemon <AhoyLemon@users.noreply.github.com>
2018-12-22 22:38:28 -0600

    Fanart grid, half done.

--- site/templates/fanart.php
+++ site/templates/fanart.php
@@ -4,20 +4,30 @@
 
     <h1 class="fanart-headline"><?php echo $page->page_headline(); ?></h1>
     
-    <ul class="fanart-list">
+    <ul class="fanart-grid">
         
       <?php foreach ($page->images()->shuffle() as $fanart): ?>
         <?php $fa = explode("-", $fanart->filename()); ?>
         <?php 
           if (count($fa) > 2) { 
-          $x = explode('.', $fa[2]); $slug = $x[0];
+          $x = explode('.', $fa[2]); 
+          $slug = $x[0];
           } else { $slug = "episode"; }
         ?>
-        <li>
-          <a href="/<?php echo $slug; ?>/<?php echo $fa[0]; ?>#AdditionalFun" class="fanart-link" title="<?= $slug; ?> <?= $fa[0]; ?>">
-            <figure style="background-image:url(<?= $fanart->crop(250, 250)->url(); ?>);">&nbsp;</figure>
-          </a>
-        </li>
+        <a href="/<?php echo $slug; ?>/<?php echo $fa[0]; ?>#AdditionalFun" class="fanart-link" title="<?= $slug; ?> <?= $fa[0]; ?>">
+          <figure>
+            <img src="<?= $fanart->crop(250, 250)->url(); ?>" />
+            <?php /* <img src="<?= $fanart->url(); ?>"> */ ?>
+          </figure>
+          <figcaption>
+            <summary>
+              <div class="artist">
+                <?php $artistName = explode('.',$fa[count($fa) - 1])[0]; ?>
+                <span class="name"><?= $artistName; ?></span>
+              </div>
+            </summary>
+          </figcaption>
+        </a>
       <?php endforeach; ?>
         
       </ul>
