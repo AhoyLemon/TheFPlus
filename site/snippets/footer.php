3df6cf07b7c4a319c4f6c4438799d27a4a36ba8f
Lemon <citrusmirakel@gmail.com>
2018-12-29 14:23:49 -0600

    Let's build another serviceworker why not

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,21 +1,23 @@
   <footer class="footer">
-    <script src="<?php echo url::home() ?>/assets/js/thefplus.js?v=01.06.18"></script>
+    <script src="<?php echo url::home() ?>/assets/js/thefplus.js"></script>
     <?php snippet('analytics') ?>
 
-    <?php /*
     <script type="text/javascript">
-      // Register the service worker
-      if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
-          // Registration was successful
-          console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }).catch(function(err) {
-          // registration failed :(
-          console.log('ServiceWorker registration failed: ', err);
+      //This is the "Offline page" service worker
+
+      //Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
+      if (navigator.serviceWorker.controller) {
+        console.log('[PWA Builder] active service worker found, no need to register')
+      } else {
+        //Register the ServiceWorker
+        navigator.serviceWorker.register('<?= $site->url(); ?>/service-worker.js', {
+          scope: './'
+        }).then(function(reg) {
+          console.log('Service worker has been registered for scope:'+ reg.scope);
         });
       }
+
     </script>
-    */ ?>
 
     <script src="<?php echo url::home() ?>/assets/js/newurl.js"></script>
   </footer>
