b4d5ba4840c4bdbdf1d40db0ce6078274bc604c4
Lemon <citrusmirakel@gmail.com>
2019-01-02 14:42:37 -0600

    mobile break is now 980px
    This closes #40

--- site/snippets/briefs.php
+++ site/snippets/briefs.php
@@ -23,11 +23,19 @@
           $category = "merch";
         }
       }
+
+
+      if (($pageCount < 1 && $i == 1) || ($pageCount > 1 && $i == 7)) {
+        $doubleSize = true;
+      } else {
+        $doubleSize = false;
+      }
+
     ?>
 
 
-    <?php if (($pageCount < 1 && $i == 1) || ($pageCount > 1 && $i == 7)) { ?>
-      <a class="brief double-size" href="<?= $article->url(); ?>" title="<?= $altText; ?>">
+    <?php if ($doubleSize == true) { ?>
+      <a class="brief double-size <?= $briefType; ?>" href="<?= $article->url(); ?>" title="<?= $altText; ?>">
         <div class="inner">
 
           <?php /* Brief Image */ ?>
@@ -100,105 +108,104 @@
 
         </div>
       </a>
+    <?php }?>
 
-    <?php } else { ?>
-      <a class="brief <?= $briefType; ?>" href="<?= $article->url(); ?>" title="<?= $altText; ?>">
-        <div class="inner">
+    <a class="brief <?= $briefType; ?> <?php if ($doubleSize == true) { echo 'has-double-size'; } ?>" href="<?= $article->url(); ?>" title="<?= $altText; ?>">
+      <div class="inner">
 
-          <?php /* Brief Image */ ?>
-          <?php if ($briefType != "wrote") { ?>
-            <figure>
-              <?php if ($article->cover()->isNotEmpty()) { ?>
-                <img src="<?= $article->cover()->toFile()->url(); ?>" alt="<?= $article->title(); ?>" class="cover<?php if ($article->cover()->toFile()->extension() == "png") { echo ' no-shadow'; } ?>" />
-              <?php } else if ($article->image()->isNotEmpty())  { ?>
-                <img src="<?= $article->image()->url(); ?>" alt="<?= $article->title(); ?>" class="cover<?php if ($article->image()->extension() == "png") { echo ' no-shadow'; } ?>" />
-              <?php } ?>
-            </figure>
-          <?php } ?>
+        <?php /* Brief Image */ ?>
+        <?php if ($briefType != "wrote") { ?>
+          <figure>
+            <?php if ($article->cover()->isNotEmpty()) { ?>
+              <img src="<?= $article->cover()->toFile()->url(); ?>" alt="<?= $article->title(); ?>" class="cover<?php if ($article->cover()->toFile()->extension() == "png") { echo ' no-shadow'; } ?>" />
+            <?php } else if ($article->image()->isNotEmpty())  { ?>
+              <img src="<?= $article->image()->url(); ?>" alt="<?= $article->title(); ?>" class="cover<?php if ($article->image()->extension() == "png") { echo ' no-shadow'; } ?>" />
+            <?php } ?>
+          </figure>
+        <?php } ?>
 
 
-          <?php /* Title */ ?>
-          <div class="name-and-title">
-            <h2 class="title"><?= $article->title(); ?></h2>
+        <?php /* Title */ ?>
+        <div class="name-and-title">
+          <h2 class="title"><?= $article->title(); ?></h2>
+        </div>
+
+        <?php if ($briefType == "wrote" || $briefType == "also-made") { ?>
+          <figcaption>
+            <div class="description">
+              <?php if ($briefType == "wrote") {
+                echo excerpt($article->text(), 420);
+              } else if ($briefType == "also-made") {
+                echo excerpt($article->text(), 220);
+              } ?>
+            </div>
+          </figcaption>
+        <?php } ?>
+
+        <?/* Category */ ?>
+        <?php if ($briefType == 'also-made') { ?>
+          <div class="category">
+            <span class="category-tag">
+              <?= $category; ?>
+            </span>
           </div>
+        <?php } ?>
 
-          <?php if ($briefType == "wrote" || $briefType == "also-made") { ?>
-            <figcaption>
-              <div class="description">
-                <?php if ($briefType == "wrote") {
-                  echo excerpt($article->text(), 420);
-                } else if ($briefType == "also-made") {
-                  echo excerpt($article->text(), 220);
-                } ?>
-              </div>
-            </figcaption>
-          <?php } ?>
-
-          <?/* Category */ ?>
-          <?php if ($briefType == 'also-made') { ?>
+        <?/* Episode # */ ?>
+        <?php if ($briefType == 'episode') { ?>
+          <?php if ((int)$article->uid()) { ?>
             <div class="category">
               <span class="category-tag">
-                <?= $category; ?>
+                  ep #<?= $article->uid(); ?>
               </span>
             </div>
           <?php } ?>
+        <?php } ?>
 
-          <?/* Episode # */ ?>
-          <?php if ($briefType == 'episode') { ?>
-            <?php if ((int)$article->uid()) { ?>
-              <div class="category">
-                <span class="category-tag">
-                   ep #<?= $article->uid(); ?>
-                </span>
-              </div>
+
+        <?/* Subject */ ?>
+        <?php if ($article->featured_site()->isNotEmpty()) { ?>
+          <div class="subject">
+            <?php if (strpos($article->featured_site(), ',') == false) { ?>
+              <span>reading</span> <strong class="source"><?= $article->featured_site(); ?></strong>
+            <?php } else { ?>
+              </span>reading</span> 
+              <?php $contentSource = explode(",", $article->featured_site()); ?>
+              <ul>
+                <?php foreach($contentSource as $key => $s) { ?>
+                  <li class="source"><?= $s ?></li>
+                <?php } ?>
+              </ul>
             <?php } ?>
-          <?php } ?>
+          </div>
+        <?php } ?>
 
-
-          <?/* Subject */ ?>
-          <?php if ($article->featured_site()->isNotEmpty()) { ?>
-            <div class="subject">
-              <?php if (strpos($article->featured_site(), ',') == false) { ?>
-                <span>reading</span> <strong class="source"><?= $article->featured_site(); ?></strong>
-              <?php } else { ?>
-                </span>reading</span> 
-                <?php $contentSource = explode(",", $article->featured_site()); ?>
-                <ul>
-                  <?php foreach($contentSource as $key => $s) { ?>
-                    <li class="source"><?= $s ?></li>
-                  <?php } ?>
-                </ul>
-              <?php } ?>
-            </div>
-          <?php } ?>
-
-          <?/* Cast */ ?>
-          <?php if ($article->cast()->isNotEmpty()) { ?>
-            <div class="cast">
-              <?php if ($article->cast()->isNotEmpty()) {
-                $persons = explode(",", $article->cast());
-              ?>
-                <span>with</span>
-                <ul>
-                  <?php foreach($persons as $key => $person) { ?>
-                    <li><?= $person ?></li>
-                  <?php } ?>
-                </ul>
-              <?php } ?>
-            </div>
-          <?php } ?>
+        <?/* Cast */ ?>
+        <?php if ($article->cast()->isNotEmpty()) { ?>
+          <div class="cast">
+            <?php if ($article->cast()->isNotEmpty()) {
+              $persons = explode(",", $article->cast());
+            ?>
+              <span>with</span>
+              <ul>
+                <?php foreach($persons as $key => $person) { ?>
+                  <li><?= $person ?></li>
+                <?php } ?>
+              </ul>
+            <?php } ?>
+          </div>
+        <?php } ?>
 
 
 
-        </div>
-        <time class="timebox">
-          <span class="day"><?= date('d', $article->date()); ?></span>
-          <span class="month"><?= date('M', $article->date()); ?></span>
-          <span class="year"><?= date('Y', $article->date()); ?></span>
-        </time>
-    
-      </a>
-    <?php } ?>
+      </div>
+      <time class="timebox">
+        <span class="day"><?= date('d', $article->date()); ?></span>
+        <span class="month"><?= date('M', $article->date()); ?></span>
+        <span class="year"><?= date('Y', $article->date()); ?></span>
+      </time>
+  
+    </a>
     
   <?php } ?>
 
