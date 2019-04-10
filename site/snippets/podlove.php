963a40489bab80aabd24468c897445e02cc47b22
Lemon <3413200+AhoyLemon@users.noreply.github.com>
2019-04-10 11:39:00 -0500

    Chaptering POC

--- site/snippets/podlove.php
+++ site/snippets/podlove.php
@@ -20,6 +20,8 @@
         publicationDate: '<?php echo $page->date('Y-m-d'); ?>T<?php echo $page->time(); ?>+06:00',
         <?php if ($page->cover()->isNotEmpty()) { ?>
           poster: '<?= $page->cover()->toFile()->url(); ?>',
+        <?php } else if ($page->image()) { ?>
+          poster: '<?= $page->image()->url(); ?>',
         <?php } ?>
         show: {
             title: '<?= $site->title(); ?>',
@@ -29,33 +31,14 @@
             url: '<?= $site->url(); ?>'
         },
         duration: '<?= $page->runtime(); ?>',
-        /*
-        chapters: [
-            { start:"00:00:00", title: 'Intro'},
-            { start:"00:01:39", title: 'Begrüßung'},
-            { start:"00:04:58", title: 'IETF Meeting Netzwerk'},
-            { start:"00:18:37", title: 'Kalender'},
-            { start:"00:33:40", title: 'Freak Show Bingo'},
-            { start:"00:35:37", title: 'Wikipedia'},
-            { start:"01:17:26", title: 'iPhone Akkukalibration'},
-            { start:"01:24:55", title: 'Alte iPads und iPod touches'},
-            { start:"01:31:02", title: 'Find My Friends'},
-            { start:"01:41:46", title: 'iPhone Music Player'},
-            { start:"01:56:13", title: 'Apple Watch'},
-            { start:"02:11:51", title: 'Kommandozeile: System Appreciation'},
-            { start:"02:23:10", title: 'Sound und Design für Games'},
-            { start:"02:24:59", title: 'Kommandozeile: Remote Deployment'},
-            { start:"02:32:37", title: 'Kommandozeile: Man Pages'},
-            { start:"02:44:31", title: 'Kommandozeile: screen vs. tmux'},
-            { start:"02:58:02", title: 'Star Wars: Machete Order & Phantom Edit'},
-            { start:"03:20:05", title: 'Kopfhörer-Ersatzteile'},
-            { start:"03:23:39", title: 'Dante'},
-            { start:"03:38:03", title: 'Dante Via'},
-            { start:"03:45:33", title: 'Internet of Things Security'},
-            { start:"03:56:11", title: 'That One Privacy Guy\'s VPN Comparison Chart'},
-            { start:"04:10:00", title: 'Ausklang'}
-        ],
-        */
+
+        <?php if ($page->chapters_toggle() == "yes" && $page->chapters()->isNotEmpty()) { ?>
+          chapters: [
+            <?php foreach ($page->chapters()->toStructure() as $chapter) { ?>
+              { start:"<?= $chapter->timestamp(); ?>", title: '<?= $chapter->name(); ?>'},
+            <?php } ?>
+          ],
+        <?php } ?>
         audio: [
           {
             url: '<?= $site->url(); ?>/podcasts/<?= $page->episode_file(); ?>',
@@ -64,12 +47,7 @@
             title: 'Audio MP3'
           }
         ],
-        /*
-        reference: {
-            config: '//podlove-player.surge.sh/fixtures/example.json',
-            share: '//podlove-player.surge.sh/share'
-        },
-        */
+
         <?php $persons = explode(",", $page->cast()); ?>
         contributors: [
 
@@ -101,7 +79,7 @@
         'tabFiles',
         //'tabAudio',
         'tabShare',
-        'poster',
+        //'poster',
         'showTitle',
         'episodeTitle',
         'subtitle',
