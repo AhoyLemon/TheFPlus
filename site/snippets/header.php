74c248f3eae434091bec26b28adabb7928b643f9
Lemon <3413200+AhoyLemon@users.noreply.github.com>
2019-05-28 10:05:31 -0500

    Cachebust
    From #49

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -64,7 +64,7 @@
   <!-- Font -->
   <link href="https://fonts.googleapis.com/css?family=Noto+Serif+TC:300,400,700" rel="stylesheet">
   <!-- Master CSS -->
-  <?= css('assets/css/thefplus.css?updated=2019-04-18'); ?>
+  <?= css('assets/css/thefplus.css?updated=2019-05-28'); ?>
 
 </head>
 <body>
