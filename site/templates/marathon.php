04f48c48b8f485e9610f0c812b0e35b8e6a14591
Lemon <3413200+AhoyLemon@users.noreply.github.com>
2019-05-28 12:05:24 -0500

    Had to redo marathon layout.

--- site/templates/marathon.php
+++ site/templates/marathon.php
@@ -6,45 +6,57 @@
   <meta http-equiv="last-modified" content="<?php echo $page->modified('Y-m-d@H:i:s'); ?>" />
   
   <div class="full marathon-block">
-    <h1><?php echo $page->title(); ?></h1>
-    
-    <div class="recorded">
-      <time>
-        Recorded: 
-        <span class="date">
-          <?php echo date('l, F jS Y', strtotime($page->record_date_start())); ?>
-        </span>
-        @
-        <span class="time">
-          <?php echo date("g:ia", strtotime($page->record_time_start())); ?>
-        </span>
-        - 
-        <span class="time">
-          <?php echo date("g:ia", strtotime($page->record_time_end())); ?>
-        </span>
-        CST
-      </time>
+
+    <div class="marathon-overview">
+
+      <figure>
+        <?php if($page->cover() != "") { ?>
+          <img itemprop="image" src="<?php echo $page->url() ?>/<?php echo $page->cover()->filename() ?>" class="cover" alt="F Plus Episode <?php echo $page->uid() ?>">
+        <?php } else if($image = $page->image()) { ?>
+          <img itemprop="image" src="<?php echo $page->url() ?>/<?php echo $image->filename() ?>" class="cover" alt="F Plus Episode <?php echo $page->uid() ?>">
+        <?php } ?>
+      </figure>
+      <h1><?php echo $page->title(); ?></h1>
+      
+      <?php if ($page->text() != "") { ?>
+        <summary class="info-block hours-leadin">
+
+          <div class="recorded">
+            <time>
+              Recorded: 
+              <span class="date">
+                <?php echo date('l, F jS Y', strtotime($page->record_date_start())); ?>
+              </span>
+              @
+              <span class="time">
+                <?php echo date("g:ia", strtotime($page->record_time_start())); ?>
+              </span>
+              - 
+              <span class="time">
+                <?php echo date("g:ia", strtotime($page->record_time_end())); ?>
+              </span>
+              CST
+            </time>
+          </div>
+          
+          <div class="released">
+            <time>
+              Released: 
+              <span class="date">
+                <?php echo date('l, F jS Y', $page->date()); ?>
+              </span>
+              @
+              <span class="time">
+                <?php echo date("g:ia", strtotime($page->time())); ?>
+              </span>
+              CST
+            </time>
+          </div>
+
+          <?php echo $page->text()->kirbytext() ?>
+        </summary>
+      <?php } ?>
     </div>
-    
-    <div class="released">
-      <time>
-        Released: 
-        <span class="date">
-          <?php echo date('l, F jS Y', $page->date()); ?>
-        </span>
-        @
-        <span class="time">
-          <?php echo date("g:ia", strtotime($page->time())); ?>
-        </span>
-        CST
-      </time>
-    </div>
-    
-    <?php if ($page->text() != "") { ?>
-      <summary class="info-block hours-leadin">
-        <?php echo $page->text()->kirbytext() ?>
-      </summary>
-    <?php } ?>
     
     <div class="marathon-hours">
       <?php foreach ($page->hours()->toStructure() as $hour) { ?>
@@ -53,16 +65,32 @@
 
             <header>
               <h2 class="block-title">HOUR <?php echo $hour->hour(); ?>: <?php echo $hour->name(); ?></h2>
+              <?php /*
               <h5>
+                <?php if ($hour->document_link()) { ?>
+                  <a href="<?php echo $hour->document_link() ?>" title="Read <?php echo $hour->provider() ?>'s document"  target="_blank">
+                    <span>Document</span>
+                  </a>
+                <?php } ?>
                 provided by 
                 <?php echo $hour->provider(); ?>
               </h5>
+              */ ?>
             </header> 
             <div class="video-holder">
               <iframe width="560" height="315" src="https://www.youtube.com/embed/<?php echo $hour->youtube_url(); ?>?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
             </div>
 
             <div class="detail-holder">
+              <h5>
+                <?php if ($hour->document_link()) { ?>
+                  <a href="<?php echo $hour->document_link() ?>" title="Read <?php echo $hour->provider() ?>'s document"  target="_blank">
+                    <span>Document</span>
+                  </a>
+                <?php } ?>
+                provided by 
+                <?php echo $hour->provider(); ?>
+              </h5>
               <ul class="cast authors ridiculists info-block">
                 <?php 
                   $readers = explode(",", $hour->cast());
@@ -97,13 +125,13 @@
                 </div>
               <?php } ?>
               <summary>
-                <?php if ($hour->provider() != "" && $hour->document_link() != "") { ?>
+                <?php /* if ($hour->provider() != "" && $hour->document_link() != "") { ?>
                   <a class="action read" href="<?php echo $hour->document_link() ?>" title="Read <?php echo $hour->provider() ?>'s document"  target="_blank">
                     <svg viewBox="0 0 100 100">
                       <use xlink:href="#IconDocument"></use>
                     </svg>
                   </a>
-                <?php } ?>
+                <?php } */ ?>
                 <?php echo $hour->text()->kirbytext(); ?>
               </summary>
             </div>
