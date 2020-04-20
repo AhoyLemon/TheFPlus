0049e9d707cfac54ce6843d131e90a196e9055bc
Lemon <3413200+AhoyLemon@users.noreply.github.com>
2020-04-20 12:55:29 -0500

    Crosslink to ballpit

--- site/snippets/podlove.php
+++ site/snippets/podlove.php
@@ -126,19 +126,6 @@
 
 </script>
 
-<div class="document-link-holder">
-  <?php if ($page->document_link()->isNotEmpty()) { ?>
-    <a itemprop="citation" class="action read" href="<?php echo $page->document_link() ?>" title="Read <?php echo $page->provider() ?>'s document"  target="_blank">
-      <svg viewBox="0 0 100 100">
-      <path d="M65.3 57H44.2v-4.6h21l.1 4.6zm0-10H44.2v-4.6h21l.1 4.6zm0-10H44.2v-4.6h21l.1 4.6zM29.1 75.3V24.4h42.8v33.4c0 9.7-11.9 5.8-11.9 5.8s3.6 11.7-5.4 11.7H29.1zM78 58.4V18.3H23v63.1h31.7c10.4 0 23.3-13.6 23.3-23zM37.8 32c-1.5 0-2.7 1.2-2.7 2.7 0 1.5 1.2 2.7 2.7 2.7s2.7-1.2 2.7-2.7c-.1-1.5-1.3-2.7-2.7-2.7zm0 10.1c-1.5 0-2.7 1.2-2.7 2.7 0 1.5 1.2 2.7 2.7 2.7s2.7-1.2 2.7-2.7c-.1-1.5-1.3-2.7-2.7-2.7zm0 9.9c-1.5 0-2.7 1.2-2.7 2.7 0 1.5 1.2 2.7 2.7 2.7s2.7-1.2 2.7-2.7c-.1-1.5-1.3-2.7-2.7-2.7z"/>
-      </svg>
-      <span class="label go-right">Read <?= str_replace(",", " & ", $page->provider()); ?>'s document</span>
-    </a>
-  <?php } ?>
-</div>
-
-
-
 <?php if ($page->chapters_toggle() == "yes" && $page->chapters()->isNotEmpty()) { ?>
   <dl class="chapters-info">
     <dt>
