a41ebffd84e8a107362fb6faf0eeb70362b7e91e
Lemon <3413200+AhoyLemon@users.noreply.github.com>
2020-09-08 20:25:37 -0500

    Made fanart anchors
    Also blog improvements.
    
    This closes #78

--- site/tags/fanart.php
+++ site/tags/fanart.php
@@ -10,3 +10,15 @@ kirbytext::$tags['fanart'] = array(
     return '<figure class="fanart"> <img src="https://thefpl.us/fanart/' . $picture . '" /> <figcaption>by ' . $artist . '</figcaption></figure>';
   }
 );
+
+
+kirbytext::$tags['img'] = array(
+  'attr' => array(
+    'caption'
+  ),
+  'html' => function($tag) {
+    $picture = $tag->attr('fanart');
+    $artist  = $tag->attr('by', 'someone');
+    return '<figure class="fanart"> <img src="https://thefpl.us/fanart/' . $picture . '" /> <figcaption>by ' . $artist . '</figcaption></figure>';
+  }
+);
