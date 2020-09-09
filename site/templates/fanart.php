c50af3062908ce6a61d720179754da03d3097e2f
Lemon <3413200+AhoyLemon@users.noreply.github.com>
2020-09-08 22:32:41 -0500

    Blog post
    This is pretty everything except Mox.

--- site/templates/fanart.php
+++ site/templates/fanart.php
@@ -38,25 +38,32 @@
       <h1 class="fanart-headline"><?php echo $page->page_headline(); ?></h1>
 
 
-      <div class="sort-navigation">
-        Sort by
-        <a class="switch-sort <?php if ($sortType == "artist") { echo 'active'; } ?>" href="<?= $site->find('fanart')->url(); ?>/sort;artist">Artist↓</a>
-        <a class="switch-sort <?php if ($sortType == "artist-reverse") { echo 'active'; } ?>" href="<?= $site->find('fanart')->url(); ?>/sort;artist-reverse">Artist↑</a>
-        <a class="switch-sort <?php if ($sortType == "episode") { echo 'active'; } ?>" href="<?= $site->find('fanart')->url(); ?>/sort;episode">Episode↓</a>
-        <a class="switch-sort <?php if ($sortType == "episode-reverse") { echo 'active'; } ?>" href="<?= $site->find('fanart')->url(); ?>/sort;episode-reverse">Episode↑</a>
-        <a class="switch-sort <?php if ($sortType == "random") { echo 'active'; } ?>" href="<?= $site->find('fanart')->url(); ?>/sort;random">Random!!!</a>
+      <div class="fanart-options sort-navigation">
+        <div class="label">
+          Sort all art by 
+        </div>
+        <div class="options">
+          <a class="switch-sort <?php if ($sortType == "artist") { echo 'active'; } ?>" href="<?= $site->find('fanart')->url(); ?>/sort;artist">Artist↓</a>
+          <a class="switch-sort <?php if ($sortType == "artist-reverse") { echo 'active'; } ?>" href="<?= $site->find('fanart')->url(); ?>/sort;artist-reverse">Artist↑</a>
+          <a class="switch-sort <?php if ($sortType == "episode") { echo 'active'; } ?>" href="<?= $site->find('fanart')->url(); ?>/sort;episode">Episode↓</a>
+          <a class="switch-sort <?php if ($sortType == "episode-reverse") { echo 'active'; } ?>" href="<?= $site->find('fanart')->url(); ?>/sort;episode-reverse">Episode↑</a>
+          <a class="switch-sort <?php if ($sortType == "random") { echo 'active'; } ?>" href="<?= $site->find('fanart')->url(); ?>/sort;random">Random!!!</a>
+        </div>
       </div>
 
-      <div class="artist-filters">
-        Featured artists:
-        <?php foreach (explode(',', $page->featured_artists()) as $fartist) { ?>
-          <a class="filter-link <?php if (param('artist') == $fartist) { echo ' active'; } ?>"
-            href="<?= $site->find('fanart')->url() . '/artist;' . $fartist; ?>"
-          >
-            <?= $fartist; ?>
-          </a>
-        <?php } ?>
-
+      <div class="fanart-options artist-filters">
+        <div class="label">
+          Featured artists:
+        </div>
+        <div class="options">
+          <?php foreach (explode(',', $page->featured_artists()) as $fartist) { ?>
+            <a class="filter-link <?php if (param('artist') == $fartist) { echo ' active'; } ?>"
+              href="<?= $site->find('fanart')->url() . '/artist;' . $fartist; ?>"
+            >
+              <?= $fartist; ?>
+            </a>
+          <?php } ?>
+        </div>
       </div>
 
       <?php snippet('fanart-thumbnails',  [ 'fanartArray' => $fanartSort ]) ?>
