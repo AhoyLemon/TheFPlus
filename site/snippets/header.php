d09c843e3ce96b48826e8990f78e8be8f4409ced
Lemon <3413200+AhoyLemon@users.noreply.github.com>
2020-12-21 22:54:56 -0600

    Music Used style tweak.

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -64,7 +64,7 @@
   <!-- Font -->
   <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+TC:wght@300;400;700&display=swap" rel="stylesheet">
   <!-- Master CSS -->
-  <?= css('assets/css/thefplus.css?updated=2020-12-07'); ?>
+  <?= css('assets/css/thefplus.css?updated=2020-12-21'); ?>
 
 </head>
 <body>
