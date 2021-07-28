d3a1fc0546d1daaaf0e3a208a86cd7056107fc55
Lemon <3413200+AhoyLemon@users.noreply.github.com>
2021-07-27 19:31:40 -0500

    I don't like those underlined links.

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -11,7 +11,7 @@
   <!-- Font -->
   <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+TC:wght@300;400;700&display=swap" rel="stylesheet">
   <!-- Master CSS -->
-  <?= css('assets/css/thefplus.css?updated=2020-03-03'); ?>
+  <?= css('assets/css/thefplus.css?updated=2021-07-27'); ?>
   <?php snippet('meta') ?>
 </head>
 <body>
