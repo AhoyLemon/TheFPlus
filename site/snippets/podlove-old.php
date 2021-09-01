436f2d1448088608e168f921c881ef885305b2dd
Lemon <3413200+AhoyLemon@users.noreply.github.com>
2021-08-31 22:16:13 -0500

    Pretty sure I want to abandon this idea.

---
+++ site/snippets/podlove-old.php
@@ -0,0 +1,171 @@
+<?= js('assets/js/vendor/podlove.web-player.4.5.13.js'); ?>
+
+<a name="chapters"></a>
+
+
+<div id="PodLovePlayer" class="audio-player-wrapper">
+  <audio preload="none" controls>
+    <source src="/podcasts/<?php echo $page->episode_file() ?>" type="audio/mpeg" />
+  </audio>
+</div>
+
+<?php if ( $page->chapters()->isNotEmpty()) {
+  $chapters = array();
+  foreach ($page->chapters()->toStructure() as $c) {
+
+    $a = (float)str_replace(':', '', $c->timestamp());
+    $b = str_pad($a, 6, '0', STR_PAD_LEFT);
+    $z = rtrim(chunk_split(substr($b,-6),2,':'),':');
+    $n = trim($c->name());
+    array_push($chapters, array("timestamp" => $z, "name" => $n));
+  }
+} ?>
+
+<script>
+
+  var p = window.location.pathname;
+
+  var episodeHasBeenPlayed = false;
+
+  if (window.location.pathname.includes('random')) {
+    p = window.location.pathname + " (RANDOM)";
+  }
+
+  podlovePlayer('#PodLovePlayer', {
+      title: '<?= addslashes($page->title()); ?>',
+      subtitle: 'Reading: <?= str_replace(',', '   ', $page->featured_site()); ?>',
+      summary: `<?= preg_replace( "/\r|\n/", "", $page->text()->kirbytext() ); ?>`,
+      theme: {
+        main: '#131313',
+        highlight: '#c0282d'
+      },
+      publicationDate: '<?php echo $page->date('Y-m-d'); ?>T<?php echo $page->time(); ?>+06:00',
+      <?php if ($page->cover()->isNotEmpty()) { ?>
+        poster: '<?= $page->cover()->toFile()->url(); ?>',
+      <?php } else if ($page->image()) { ?>
+        poster: '<?= $page->image()->url(); ?>',
+      <?php } ?>
+      show: {
+          title: '<?= $site->title(); ?>',
+          summary: '<?= $site->description(); ?>',
+          poster: 'https://thefpl.us/assets/images/og-image.png',
+          link: '<?= $site->url(); ?>'
+      },
+      <?php if ($page->runtime()->isNotEmpty()) { ?>
+        duration: '<?= $page->runtime(); ?>',
+      <?php } ?>
+      link: '<?= $page->url(); ?>',
+      <?php if ($page->chapters_toggle() == "yes" && $page->chapters()->isNotEmpty()) { ?>
+        chapters: [
+          <?php foreach ($chapters as $chapter) { ?>
+            { start:'<?= $chapter['timestamp']; ?>', title: '<?= addslashes($chapter['name']); ?>'},
+          <?php } ?>
+        ],
+      <?php } ?>
+      <?php if ($page->episode_file()->isNotEmpty()) { ?>
+        audio: [
+          {
+            url: '<?= $site->url(); ?>/podcasts/<?= $page->episode_file(); ?>',
+            mimeType: 'audio/mp3',
+            <?php if ($page->file_size()->isNotEmpty()) { ?>
+              size: <?= $page->file_size(); ?>000000,
+            <?php } ?>
+            title: 'Audio MP3'
+          }
+        ],
+      <?php } ?>
+
+      <?php if ($page->cast()->isNotEmpty()) { ?>
+      contributors: [
+        <?php $persons = explode(",", $page->cast()); ?>
+        <?php foreach($persons as $person) { ?>
+        {
+          name: '<?= addslashes($person); ?>',
+          <?php $mlink = 'meet/'.strtolower(preg_replace('/\s+/', '-', str_replace("'", "", $person))); ?>
+          <?php if ($site->find($mlink)) { ?>
+            avatar: '<?= $site->find($mlink)->image()->url(); ?>',
+          <?php } ?>
+          comment: null
+        },
+      <?php } ?>
+    <?php } ?>
+    ],
+    visibleComponents: [
+      'tabInfo',
+      <?php if ($page->chapters_toggle() == "yes" && $page->chapters()->isNotEmpty()) { ?>
+        'tabChapters',
+        'controlChapters',
+      <?php } ?>
+      'tabShare',
+      'tabFiles',
+      'showTitle',
+      'episodeTitle',
+      'progressbar',
+      'controlSteppers',
+      
+    ]
+  }).then(function (store) {
+    store.subscribe(() => {
+      const { lastAction } = store.getState()
+      
+
+      console.log(lastAction);
+      //console.log({ type: lastAction.type, payload: lastAction.payload })
+      if (lastAction.type == "PLAY") {
+        if (!episodeHasBeenPlayed) {
+          trackEvent("listen", "play", p);
+        }
+        episodeHasBeenPlayed = true;
+      }
+      
+      if (lastAction.type === "TOGGLE_TAB" && lastAction.payload == "files") {
+        if (lastAction.payload == "files") {
+          trackEvent("listen", "download", p);
+        }
+      }
+
+    });
+  });
+
+</script>
+
+<?php if ($page->chapters_toggle() == "yes" && $page->chapters()->isNotEmpty()) { ?>
+  <dl class="chapters-info">
+    <dt>
+
+      <span class="icon">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+          <path d="M13 10v82l80-41z"/>
+        </svg>
+      </span>
+
+      <?php if ($page->chapter_provider()->isNotEmpty()) { ?>
+        Chapters provided by <?= $page->chapter_provider(); ?>
+      <?php } else { ?>
+        Chapters
+      <?php } ?>
+    </dt>
+    <dd>
+      <table class="chapters">
+        <tbody>
+          <?php $i = 0; ?>
+          <?php foreach ($chapters as $c) { ?>
+            <?php $i++; ?>
+            <tr>
+              <td class="count"><?= $i; ?></td>
+              <td class="timestamp"><?= $c['timestamp'] ?></td>
+              <td class="name"><?= $c['name']; ?></td>
+            </tr>
+          <?php } ?>
+        </tbody>
+      </table>
+    </dd>
+  </dl>
+
+  <script>
+    $('dl.chapters-info dt').click(function() {
+      $(this).parent().toggleClass('expanded');
+    });
+  </script>
+
+<?php } ?>
--- site/snippets/podlove.php
+++ site/snippets/podlove.php
@@ -1,13 +1,5 @@
-<?= js('assets/js/vendor/podlove.web-player.4.5.13.js'); ?>
-
-<a name="chapters"></a>
-
-
-<div id="PodLovePlayer" class="audio-player-wrapper">
-  <audio preload="none" controls>
-    <source src="/podcasts/<?php echo $page->episode_file() ?>" type="audio/mpeg" />
-  </audio>
-</div>
+<div class="app" id="app"></div>
+<script src="//cdn.podlove.org/web-player/5.x/embed.js"></script>
 
 <?php if ( $page->chapters()->isNotEmpty()) {
   $chapters = array();
@@ -23,149 +15,290 @@
 
 <script>
 
-  var p = window.location.pathname;
+  const episode = {
+  // Configuration Version
+  version: 5,
 
-  var episodeHasBeenPlayed = false;
+  /**
+   * Show Related Information
+   */
+  show: {
+    title: "<?= $site->title(); ?>",
+    subtitle: "<?= $site->description(); ?>",
+    summary: "<?= $site->description(); ?>",
+    poster: "https://thefpl.us/assets/images/og-image.png",
+    link: "<?= $site->url(); ?>"
+  },
 
-  if (window.location.pathname.includes('random')) {
-    p = window.location.pathname + " (RANDOM)";
-  }
 
-  podlovePlayer('#PodLovePlayer', {
-      title: '<?= addslashes($page->title()); ?>',
-      subtitle: 'Reading: <?= str_replace(',', '   ', $page->featured_site()); ?>',
-      summary: `<?= preg_replace( "/\r|\n/", "", $page->text()->kirbytext() ); ?>`,
-      theme: {
-        main: '#131313',
-        highlight: '#c0282d'
-      },
-      publicationDate: '<?php echo $page->date('Y-m-d'); ?>T<?php echo $page->time(); ?>+06:00',
-      <?php if ($page->cover()->isNotEmpty()) { ?>
-        poster: '<?= $page->cover()->toFile()->url(); ?>',
-      <?php } else if ($page->image()) { ?>
-        poster: '<?= $page->image()->url(); ?>',
-      <?php } ?>
-      show: {
-          title: '<?= $site->title(); ?>',
-          summary: '<?= $site->description(); ?>',
-          poster: 'https://thefpl.us/assets/images/og-image.png',
-          link: '<?= $site->url(); ?>'
-      },
-      <?php if ($page->runtime()->isNotEmpty()) { ?>
-        duration: '<?= $page->runtime(); ?>',
-      <?php } ?>
-      link: '<?= $page->url(); ?>',
-      <?php if ($page->chapters_toggle() == "yes" && $page->chapters()->isNotEmpty()) { ?>
-        chapters: [
-          <?php foreach ($chapters as $chapter) { ?>
-            { start:'<?= $chapter['timestamp']; ?>', title: '<?= addslashes($chapter['name']); ?>'},
-          <?php } ?>
-        ],
-      <?php } ?>
-      <?php if ($page->episode_file()->isNotEmpty()) { ?>
-        audio: [
-          {
-            url: '<?= $site->url(); ?>/podcasts/<?= $page->episode_file(); ?>',
-            mimeType: 'audio/mp3',
-            <?php if ($page->file_size()->isNotEmpty()) { ?>
-              size: <?= $page->file_size(); ?>000000,
-            <?php } ?>
-            title: 'Audio MP3'
-          }
-        ],
-      <?php } ?>
 
-      <?php if ($page->cast()->isNotEmpty()) { ?>
-      contributors: [
-        <?php $persons = explode(",", $page->cast()); ?>
-        <?php foreach($persons as $person) { ?>
-        {
-          name: '<?= addslashes($person); ?>',
-          <?php $mlink = 'meet/'.strtolower(preg_replace('/\s+/', '-', str_replace("'", "", $person))); ?>
-          <?php if ($site->find($mlink)) { ?>
-            avatar: '<?= $site->find($mlink)->image()->url(); ?>',
-          <?php } ?>
-          comment: null
-        },
+  /**
+   * Episode related Information
+   */
+  title: "<?= addslashes($page->title()); ?>",
+  subtitle: "Reading: <?= str_replace(',', '   ', $page->featured_site()); ?>",
+  summary: `<?= preg_replace( "/\r|\n/", "", $page->text()->kirbytext() ); ?>`,
+  publicationDate: "<?= $page->date(); ?>",
+  //poster: "<?= $page->cover()->toFile()->url(); ?>",
+  // ISO 8601 Duration format ([hh]:[mm]:[ss].[sss]), capable of add ing milliseconds, see https://en.wikipedia.org/wiki/ISO_8601
+  <?php if ($page->runtime()->isNotEmpty()) { ?>
+    duration: '<?= $page->runtime(); ?>',
+  <?php } ?>
+  link: "<?= $page->url(); ?>",
+  /**
+   * Audio Assets
+   * - media Assets played by the audio player
+   * - format support depends on the used browser (https://en.wikipedia.org/wiki/HTML5_audio#Supported_audio_coding_formats)
+   * - also supports HLS streams
+   *
+   * Asset
+   * - url: absolute path to media asset
+   * - size: file size in  byte
+   * - (title): title to be displayed in download tab
+   * - mimeType: media asset mimeType
+   */
+  audio: [
+    {
+      url: '<?= $site->url(); ?>/podcasts/<?= $page->episode_file(); ?>',
+      <?php if ($page->file_size()->isNotEmpty()) { ?>
+        size: <?= $page->file_size(); ?>000000,
       <?php } ?>
-    <?php } ?>
+      title: "MP3 Audio (mp3)",
+      mimeType: "audio/mpeg"
+    }
+  ],
+
+
+  <?php if ($page->chapters_toggle() == "yes" && $page->chapters()->isNotEmpty()) { ?>
+    chapters: [
+      <?php foreach ($chapters as $chapter) { ?>
+        { start:'<?= $chapter['timestamp']; ?>', title: '<?= addslashes($chapter['name']); ?>', href:"", image:"" },
+      <?php } ?>
     ],
-    visibleComponents: [
-      'tabInfo',
-      <?php if ($page->chapters_toggle() == "yes" && $page->chapters()->isNotEmpty()) { ?>
-        'tabChapters',
-        'controlChapters',
-      <?php } ?>
-      'tabShare',
-      'tabFiles',
-      'showTitle',
-      'episodeTitle',
-      'progressbar',
-      'controlSteppers',
-      
+  <?php } ?>
+
+  /**
+   * Contributors
+   * - used by info and transcripts tab
+   *
+   * Contributor
+   * - id: used as a reference in transcripts
+   * - name: name of the contributor
+   * - (avatar): avatar of the contributor
+   * - (group): contributors group
+   */
+};
+
+const config = {
+  version: 5,
+
+  // player asset base path, falls back to ./
+  base: "player/",
+
+  activeTab: "chapters", // default active tab, can be set to [chapters, files, share, playlist]
+
+  theme: {
+    /**
+     * Tokens
+     * - if defined the player defaults are dropped
+     * - rgba as well as hex values are allowed
+     * - use this generator to get a direct visual feedback:
+     */
+    tokens: {
+      brand: "#87B0D4",
+      brandDark: "#0F2942",
+      brandDarkest: "#1A3A4A",
+      brandLightest: "#E5EAECFF",
+      shadeDark: "#807E7C",
+      shadeBase: "#807E7C",
+      contrast: "#000",
+      alt: "#fff"
+    },
+
+    /**
+     * Fonts
+     * - by default the system font stack is used (https://css-tricks.com/snippets/css/system-font-stack/)
+     *
+     * font:
+     * - name: font name that is used in the font stack
+     * - family: list of fonts in a fallback order
+     * - weight: font weight of the defined font
+     * - src: list of web font sources (allowed: woff, woff2, ttf, eot, svg)
+     */
+    fonts: {
+      ci: {
+        name: "RobotoBlack",
+        family: [
+          "RobotoBlack",
+          "Calibri",
+          "Candara",
+          "Arial",
+          "Helvetica",
+          "sans-serif"
+        ],
+        weight: 900,
+        src: ["./assets/roboto-black.woff2"]
+      },
+      regular: {
+        name: "FiraSansLight",
+        family: [
+          "FiraSansLight",
+          "Calibri",
+          "Candara",
+          "Arial",
+          "Helvetica",
+          "sans-serif"
+        ],
+        weight: 300,
+        src: ["./assets/fira-sans-light.woff2"]
+      },
+      bold: {
+        name: "FiraSansBold",
+        family: [
+          "FiraSansBold",
+          "Calibri",
+          "Candara",
+          "Arial",
+          "Helvetica",
+          "sans-serif"
+        ],
+        weight: 700,
+        src: ["./assets/fira-sans-normal.woff2"]
+      }
+    }
+  },
+
+  /**
+   * Subscribe Button
+   * - configuration for the subsscribe button overlay
+   * - if not defined the subscribe button won't be rendered
+   */
+  "subscribe-button": {
+    feed: "https://feeds.podlovers.org/mp3", // Rss feed
+
+    /**
+     * Clients
+     * - list of supported podcast clients on android, iOS, Windows, OSX
+     * - only available clients on the used os/platform are shown
+     * - order in list determines rendered order
+     */
+    clients: [
+      {
+        id: "apple-podcasts",
+        service: "id1523714548" // https://podcasts.apple.com/podcast/[service]
+      },
+      {
+        id: "antenna-pod"
+      },
+      {
+        id: "beyond-pod"
+      },
+      // {
+      //   id: "castbox",
+      //   service: "castbox-id"
+      // },
+      {
+        id: "castro"
+      },
+      {
+        id: "clementine"
+      },
+      // {
+      //   id: "deezer",
+      //   service: "" https://www.deezer.com/en/show/[service]
+      // },
+      {
+        id: "downcast"
+      },
+      {
+        id: "google-podcasts",
+        service: "https://feeds.podlovers.org/mp3" // feed
+      },
+      {
+        id: "gpodder"
+      },
+      {
+        id: "itunes"
+      },
+      {
+        id: "i-catcher"
+      },
+      {
+        id: "instacast"
+      },
+      {
+        id: "overcast"
+      },
+      {
+        id: "player-fm"
+      },
+      {
+        id: "pocket-casts",
+        service: "TheFPlus" // feed
+      },
+      {
+        id: "pod-grasp"
+      },
+      {
+        id: "podcast-addict"
+      },
+      {
+        id: "podcast-republic"
+      },
+      {
+        id: "podcat"
+      },
+      {
+        id: "podscout"
+      },
+      {
+        id: "rss-radio"
+      },
+      // {
+      //   id: "soundcloud",
+      //   service: "", // https://soundcloud.com/[service]
+      // },
+      // {
+      //   id: "spotify",
+      //   service: "", // https://open.spotify.com/show/[service]
+      // },
+      // {
+      //   id: "stitcher",
+      //   service: "" // https://www.stitcher.com/podcast/[service]
+      // },
+      // {
+      //   id: "youtube",
+      //   service: "" // https://www.youtube.com/channel/[service]
+      // },
+      {
+        id: "rss"
+      }
     ]
-  }).then(function (store) {
-    store.subscribe(() => {
-      const { lastAction } = store.getState()
-      
+  },
 
-      console.log(lastAction);
-      //console.log({ type: lastAction.type, payload: lastAction.payload })
-      if (lastAction.type == "PLAY") {
-        if (!episodeHasBeenPlayed) {
-          trackEvent("listen", "play", p);
-        }
-        episodeHasBeenPlayed = true;
-      }
-      
-      if (lastAction.type === "TOGGLE_TAB" && lastAction.payload == "files") {
-        if (lastAction.payload == "files") {
-          trackEvent("listen", "download", p);
-        }
-      }
+  /*
+    Share Tab
+  */
+  share: {
+    /**
+     * Share Channels:
+     * - list of available channels in share tab
+     */
+    channels: [
+      "facebook",
+      "twitter",
+      "whats-app",
+      "linkedin",
+      "pinterest",
+      "xing",
+      "mail",
+      "link"
+    ],
+    sharePlaytime: true
+  }
+};
 
-    });
-  });
 
+
+  window.podlovePlayer("#app", episode, config);
 </script>
-
-<?php if ($page->chapters_toggle() == "yes" && $page->chapters()->isNotEmpty()) { ?>
-  <dl class="chapters-info">
-    <dt>
-
-      <span class="icon">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-          <path d="M13 10v82l80-41z"/>
-        </svg>
-      </span>
-
-      <?php if ($page->chapter_provider()->isNotEmpty()) { ?>
-        Chapters provided by <?= $page->chapter_provider(); ?>
-      <?php } else { ?>
-        Chapters
-      <?php } ?>
-    </dt>
-    <dd>
-      <table class="chapters">
-        <tbody>
-          <?php $i = 0; ?>
-          <?php foreach ($chapters as $c) { ?>
-            <?php $i++; ?>
-            <tr>
-              <td class="count"><?= $i; ?></td>
-              <td class="timestamp"><?= $c['timestamp'] ?></td>
-              <td class="name"><?= $c['name']; ?></td>
-            </tr>
-          <?php } ?>
-        </tbody>
-      </table>
-    </dd>
-  </dl>
-
-  <script>
-    $('dl.chapters-info dt').click(function() {
-      $(this).parent().toggleClass('expanded');
-    });
-  </script>
-
-<?php } ?>
