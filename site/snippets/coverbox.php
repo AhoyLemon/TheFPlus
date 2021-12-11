f588a123bbf20bd2bf4614028ea03469881aa59c
Lemon <3413200+AhoyLemon@users.noreply.github.com>
2021-12-11 13:30:13 -0600

    Episodes page improvements.
    This closes #95

--- site/snippets/coverbox.php
+++ site/snippets/coverbox.php
@@ -1,21 +1,39 @@
 <?php if($image = $article->image()) { ?>
   <a class="tile" href="<?php echo $article->url() ?>" title="<?php echo $article->title() ?>">
-    <?php if ($article->cover() != "") { ?>
-      <img src="<?php echo $article->url() ?>/<?php echo $article->cover()->filename() ?>" class="cover" loading="lazy" height="400" width="400" />
-    <?php } else { ?>
-      <img src="<?php echo $article->url() ?>/<?php echo $image->filename() ?>" class="cover" loading="lazy" height="400" width="400" />
-    <?php } ?>
+    <figure>
+
+      <div class="cover-loading">
+        <div class="episode-summary">
+          <?php if (is_numeric($article->uid())) {  ?>
+            <div class="episode-number">
+              <?= $article->uid(); ?>
+            </div>
+          <?php } ?>
+          <div class="episode-title">
+            <?php echo $article->title(); ?>
+          </div>
+        </div>
+      </div>
+
+      <!-- <img src="" class="cover" loading="lazy" height="400" width="400" /> -->
+
+      <?php if ($article->cover() != "") { ?>
+        <img src="<?php echo $article->url() ?>/<?php echo $article->cover()->filename() ?>" class="cover" loading="lazy" height="400" width="400" />
+      <?php } else { ?>
+        <img src="<?php echo $article->url() ?>/<?php echo $image->filename() ?>" class="cover" loading="lazy" height="400" width="400" />
+      <?php } ?>
     
-    <figcaption>
-      <summary>
-      <h4 class="title">
-        <?php if (is_numeric($article->uid())) { echo $article->uid() . ": "; } ?>
-        <span><?php echo $article->title() ?></span>
-      </h4>
-        <p><?php echo excerpt($article->text(), 185) ?></p>
-      </summary>
-    </figcaption>
-    </summary>
+      <figcaption>
+        <summary>
+        <h4 class="title">
+          <?php if (is_numeric($article->uid())) { echo $article->uid() . ": "; } ?>
+          <span><?php echo $article->title() ?></span>
+        </h4>
+          <p><?php echo excerpt($article->text(), 185) ?></p>
+        </summary>
+      </figcaption>
+
+    </figure>
   </a>
 <?php } else { ?>
   <a href="<?php echo $article->url() ?>" class="no-image" title="<?php echo $article->title() ?>">
--- site/snippets/header.php
+++ site/snippets/header.php
@@ -11,7 +11,7 @@
   <!-- Font -->
   <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+TC:wght@300;400;700&display=swap" rel="stylesheet">
   <!-- Master CSS -->
-  <?= css('assets/css/thefplus.css?updated=2021-07-27'); ?>
+  <?= css('assets/css/thefplus.css?updated=2021-12-11'); ?>
   <?php snippet('meta') ?>
 </head>
 <body>
